4c3a61cb1644f44a33e77f434841fd5b8edd9019
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-15 12:31:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for(let i = 0; i < 25; i++) {
-    var s = (i === 0)? '' : 's'
+    var s = (i === 1)? '' : 's'
     array.push("I am " + i + " strange loop" + s + ".")
   }
   return array
